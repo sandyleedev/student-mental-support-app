c5bc4fff798847825d099b474d0c5ba94e82a2c5
Seungji(Sandy) Lee <sandyleedev@gmail.com>
2026-02-17 14:18:17 +0000

    [BUS-34] feat: add POST /api/threads and split routes

--- backend/app/__init__.py
+++ backend/app/__init__.py
@@ -1,13 +1,24 @@
 from flask import Flask
 from flask_cors import CORS
 from flasgger import Swagger
+from flask_sqlalchemy import SQLAlchemy
 
-from app.config import SECRET_KEY, DEBUG, CORS_ORIGINS
+from app.config import (
+    SECRET_KEY,
+    DEBUG,
+    CORS_ORIGINS,
+    SQLALCHEMY_DATABASE_URI,
+    SQLALCHEMY_TRACK_MODIFICATIONS,
+)
 
 app = Flask(__name__)
 app.config["SECRET_KEY"] = SECRET_KEY
 app.config["DEBUG"] = DEBUG
+app.config["SQLALCHEMY_DATABASE_URI"] = SQLALCHEMY_DATABASE_URI
+app.config["SQLALCHEMY_TRACK_MODIFICATIONS"] = SQLALCHEMY_TRACK_MODIFICATIONS
 CORS(app, origins=CORS_ORIGINS, supports_credentials=True)
+
+db = SQLAlchemy(app)
 
 Swagger(
     app,
@@ -21,4 +32,5 @@ Swagger(
     },
 )
 
-from app import routes
+from app import models  # noqa: E402, F401
+from app import routes  # noqa: E402
