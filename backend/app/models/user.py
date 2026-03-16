04589f9ea420206d5e00cb5289f5ce587be77aa7
sandyleedev <sandyleedev@gmail.com>
2026-03-16 15:15:29 +0000

    [BUS-63] fix(db): add user credentials and include email in users API

--- backend/app/models/user.py
+++ backend/app/models/user.py
@@ -7,6 +7,8 @@ class User(db.Model):
     id = db.Column(db.BigInteger, primary_key=True, autoincrement=True)
     role = db.Column(db.String(20), nullable=False)
     name = db.Column(db.String(255), nullable=False)
+    email = db.Column(db.String(255), nullable=False, unique=True)
+    password = db.Column(db.String(255), nullable=False)
     created_at = db.Column(db.DateTime(timezone=True), server_default=db.func.now(), nullable=False)
     updated_at = db.Column(db.DateTime(timezone=True), server_default=db.func.now(), onupdate=db.func.now(), nullable=False)
 
